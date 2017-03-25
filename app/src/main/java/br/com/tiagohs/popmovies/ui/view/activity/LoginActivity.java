9bce79d997a1cdc016fc51993be6c5f96db7540b
Tiago Henrique da Silva <tiago.hsilva@al.infnet.edu.br>
2017-03-25 16:42:56 -0300

    teste

--- app/src/main/java/br/com/tiagohs/popmovies/ui/view/activity/LoginActivity.java
+++ app/src/main/java/br/com/tiagohs/popmovies/ui/view/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -187,6 +188,7 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
                     @Override
                     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
                         ViewUtils.createToastMessage(LoginActivity.this, getString(R.string.login_google_error));
+                        Log.i(TAG, "Mensagge: " + connectionResult.getErrorMessage());
 
                         setGoogleButtonVisibility(View.VISIBLE);
                         setTwitterButtonVisibility(View.VISIBLE);
